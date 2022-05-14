8eba9b1ca794391704e20cbedd1efac80e8cd60d
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2022-05-14 14:28:06 +0100

    improve screenreader accessibility

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,29 +27,33 @@
 
 <div class="flex flex-col flex-grow">
   <!-- Biography -->
-  <div class="flex flex-col items-center justify-center text-xl text-center">
+  <section class="flex flex-col items-center justify-center text-xl text-center">
     <svelte:component this={bio} />
-  </div>
+  </section>
 
   <hr />
 
   <!-- recent posts -->
-  <h1 class="flex items-baseline gap-4 mb-4">
-    <a href="#recent-posts" id="recent-posts">Recent Posts</a>
-    <ButtonLink href="/posts" size="medium" raised={false} class="opacity-60">View All</ButtonLink>
-  </h1>
-  <ul class="grid gap-4 grid-cols-1 sm:grid-cols-2 pl-0">
-    {#each recentPosts as post}
-      <li class="flex p-4 border border-slate-300 dark:border-slate-700 rounded-lg">
-        <PostPreview {post} small />
-      </li>
-    {/each}
-  </ul>
+  <section>
+    <span class="flex items-baseline gap-4">
+      <h1>
+        <a href="#recent-posts" id="recent-posts">Recent Posts</a>
+      </h1>
+      <ButtonLink href="/posts" size="medium" raised={false} class="opacity-60">View All</ButtonLink>
+    </span>
+    <ul class="grid gap-4 grid-cols-1 sm:grid-cols-2 pl-0">
+      {#each recentPosts as post}
+        <li class="flex p-4 border border-slate-300 dark:border-slate-700 rounded-lg">
+          <PostPreview {post} small />
+        </li>
+      {/each}
+    </ul>
+  </section>
 
   <hr />
 
   <!-- Page Content -->
-  <div class="flex flex-col items-center justify-center text-xl mb-8 mt-16 text-center">
+  <section class="flex flex-col items-center justify-center text-xl mb-8 mt-16 text-center">
     <svelte:component this={content} />
-  </div>
+  </section>
 </div>
